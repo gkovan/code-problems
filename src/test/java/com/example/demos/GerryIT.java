8ad2bb6989b9a6f39f42139c55ced1559f68ec5c
gkovan <gkovan@hotmail.com>
2022-03-18 18:32:23 -0400

    failsafe plugin working

---
+++ src/test/java/com/example/demos/GerryIT.java
@@ -0,0 +1,13 @@
+package com.example.demos;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GerryIT {
+
+    @Test
+    public void gkIntegrationTest() {
+        assertTrue(true);
+    }
+}
